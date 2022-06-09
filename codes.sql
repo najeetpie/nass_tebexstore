084ed6774155ebfa57d7200f6f7883df48df2a38
nass <104698566+nassTman@users.noreply.github.com>
2022-06-08 23:59:49 -0400

    Removed Redundant Code

--- codes.sql
+++ codes.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `codes` (
   `code` varchar(50) NOT NULL DEFAULT '',
   `packagename` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `amount` int(100) NOT NULL DEFAULT 0,
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
