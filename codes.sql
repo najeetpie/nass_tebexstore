54ad86b1b40033ed96dd79dbda49d85cd131fd4b
nass <104698566+nassTman@users.noreply.github.com>
2022-06-08 21:07:02 -0400

    Update codes.sql

--- codes.sql
+++ codes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `codes` (
   `code` varchar(50) NOT NULL DEFAULT '',
-  `packagename` varchar(255) NOT NULL,
+  `packagename` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `amount` int(100) NOT NULL DEFAULT 0,
   PRIMARY KEY (`code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
