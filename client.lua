fdd4a1376281c770100abc4ddc999ccaab714b6e
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-06-20 17:58:02 +0200

    cleanup
    - Big cleanup of code
    - Optimised code
    - Updated to newest esx-legacy syntax for getting the core object
    - Uses oxmysql now instead of mysql-async
    - Fixed indentation that was all over the place
    - Added lua 5.4 and fxv2 oal for faster native invocation
    - Moved GeneratePlate to the server side because it's faster to use that than it is to loop a callback

--- client.lua
+++ client.lua
@@ -1,28 +1,17 @@
-ESX = nil
-QBCore = nil
-local NumberCharset = {}
-local Charset = {}
+local ESX = nil
+local QBCore = nil
 
-for i = 48,  57 do table.insert(NumberCharset, string.char(i)) end
-
-for i = 65,  90 do table.insert(Charset, string.char(i)) end
-for i = 97, 122 do table.insert(Charset, string.char(i)) end
-
-Citizen.CreateThread(function()
+CreateThread(function()
 	if Config.Framework == "ESX" then
-		while ESX == nil do
-			TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-			Citizen.Wait(0)
-		end
+		ESX = exports.es_extended:getSharedObject()
 	elseif Config.Framework == "QB" then
 		QBCore = exports['qb-core']:GetCoreObject()
 	else
-		TriggerEvent("nass_tebexstore:notify", "nass_tebexstore: Framework in config is not set correctly. ")
+		TriggerEvent("nass_tebexstore:notify", "nass_tebexstore: Framework in config is not set correctly.")
 	end
 end)
 
-RegisterNetEvent('nass_tebexstore:notify')
-AddEventHandler('nass_tebexstore:notify', function(message)
+RegisterNetEvent('nass_tebexstore:notify', function(message)
 	if Config.Framework == "ESX" then
 		ESX.ShowNotification(message)
 	elseif Config.Framework == "QB" then
@@ -30,113 +19,53 @@ AddEventHandler('nass_tebexstore:notify', function(message)
 	end
 end)
 
-
-
-generatePlate = function()
-    local plate
-    local breakLoop = false
-    while true do
-        Wait(5)
-        math.randomseed(GetGameTimer())
-        if Config.SpaceInLicensePlate then
-            plate = string.upper(GetRandomLetter(Config.LicensePlateLetters).. ' '..GetRandomNumber(Config.LicensePlateNumbers))
-        else
-            plate = string.upper(GetRandomLetter(Config.LicensePlateLetters) ..GetRandomNumber(Config.LicensePlateNumbers))
-        end
-		if Config.Framework == "ESX" then
-			ESX.TriggerServerCallback('nass_tebexstore:checkPlate', function(taken)
-				if not taken then
-					breakLoop = true
-				end
-			end, plate)
-		elseif Config.Framework == "QB" then
-			QBCore.Functions.TriggerCallback('nass_tebexstore:checkPlate', function(taken)
-				if not taken then
-					breakLoop = true
-				end
-			end, plate)
-		end
-        
-        if breakLoop then
-            break
-        end
-    end
-    return plate
-end
-
-
-function GetRandomNumber(length)
-	Wait(0)
-	if length > 0 then
-		return GetRandomNumber(length - 1) .. NumberCharset[math.random(1, #NumberCharset)]
-	else
-		return ''
-	end
-end
-
-function GetRandomLetter(length)
-	Wait(0)
-	if length > 0 then
-		return GetRandomLetter(length - 1) .. Charset[math.random(1, #Charset)]
-	else
-		return ''
-	end
-end
-
-
-RegisterNetEvent('nass_tebexstore:spawnveh')
-AddEventHandler('nass_tebexstore:spawnveh', function(model)
-	
+RegisterNetEvent('nass_tebexstore:spawnveh', function(model)
 	if Config.Framework == "ESX" then
-		ESX.TriggerServerCallback('nass_tebexstore:redeemCheck', function(isLegit)
-			
-			if isLegit then
-				
-				local carExist  = false
-	
-				ESX.Game.SpawnVehicle(model, GetEntityCoords(GetPlayerPed(-1)) - vector3(0.0, 0.0, 10.0), 0.0, function(vehicle) --get vehicle info
-					if DoesEntityExist(vehicle) then
-						carExist = true
-						SetEntityVisible(vehicle, false, false)
-						SetEntityCollision(vehicle, false)
-						
-						local newPlate     = generatePlate()
-						
-						local vehicleProps = ESX.Game.GetVehicleProperties(vehicle)
-						vehicleProps.plate = newPlate
-						TriggerServerEvent('nass_tebexstore:setVehicle', vehicleProps)
-						
-						ESX.Game.DeleteVehicle(vehicle)	
-					end	
-				end, false)
-				
-				Wait(1000)
-				if not carExist then
-					TriggerServerEvent('nass_tebexstore:carNotExist')		
-				end
-			end
+		ESX.TriggerServerCallback('nass_tebexstore:redeemCheck', function(isLegit, newPlate)
+			if not isLegit or not newPlate then return end
+			local carExist = false
+
+			ESX.Game.SpawnVehicle(model, GetEntityCoords(PlayerPedId()) - vector3(0.0, 0.0, 10.0), 0.0, function(vehicle) -- Get vehicle info
+				carExist = true
+
+				SetEntityVisible(vehicle, false, false)
+				SetEntityCollision(vehicle, false, false)
+				FreezeEntityPosition(vehicle, true)
+
+				local vehicleProps = ESX.Game.GetVehicleProperties(vehicle)
+				vehicleProps.plate = newPlate
+				TriggerServerEvent('nass_tebexstore:setVehicle', vehicleProps)
+
+				ESX.Game.DeleteVehicle(vehicle)
+			end, false)
+
+			Wait(1000)
+			if carExist then return end
+			TriggerServerEvent('nass_tebexstore:carNotExist')
 		end, model)
 	elseif Config.Framework == "QB" then
-		QBCore.Functions.TriggerCallback('nass_tebexstore:redeemCheck', function(isLegit)
-			if isLegit then
-				local carExist  = false
+		QBCore.Functions.TriggerCallback('nass_tebexstore:redeemCheck', function(isLegit, newPlate)
+			if not isLegit or not newPlate then return end
+			local carExist = false
+			local vehCoords = GetEntityCoords(PlayerPedId()) - vector3(0.0, 0.0, 10.0)
 
-				QBCore.Functions.SpawnVehicle(model, function(vehicle)
-						carExist = true
-						SetEntityVisible(vehicle, false, false)
-						SetEntityCollision(vehicle, false)
-						local newPlate     = generatePlate()
-						local vehicleProps = QBCore.Functions.GetVehicleProperties(vehicle)
-						vehicleProps.plate = newPlate
-						TriggerServerEvent('nass_tebexstore:setVehicle', vehicleProps)
-						QBCore.Functions.DeleteVehicle(vehicle)	
-				end, vector4(0.0, 0.0, -10.0, 0.0), false)
-				
-				Wait(1000)
-				if not carExist then
-					TriggerServerEvent('nass_tebexstore:carNotExist')		
-				end
-			end
+			QBCore.Functions.SpawnVehicle(model, function(vehicle) -- Get vehicle info
+				carExist = true
+
+				SetEntityVisible(vehicle, false, false)
+				SetEntityCollision(vehicle, false, false)
+				FreezeEntityPosition(vehicle, true)
+
+				local vehicleProps = QBCore.Functions.GetVehicleProperties(vehicle)
+				vehicleProps.plate = newPlate
+				TriggerServerEvent('nass_tebexstore:setVehicle', vehicleProps)
+
+				QBCore.Functions.DeleteVehicle(vehicle)
+			end, vector4(vehCoords.x, vehCoords.y, vehCoords.z, 0.0), false)
+
+			Wait(1000)
+			if carExist then return end
+			TriggerServerEvent('nass_tebexstore:carNotExist')
 		end, model)
 	end
 end)
